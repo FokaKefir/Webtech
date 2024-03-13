7416de43121fb260d4d04719aa3421ef175e3292
Babos Dávid <babosdavid8@gmail.com>
2024-03-13 18:38:36 +0200

    lab04

--- 03_js_core_labor/tomb11.js
+++ 03_js_core_labor/tomb11.js
@@ -22,3 +22,65 @@ console.log("Az c elemei:")
 for (let e of c){
     console.log(e);
 } 
+
+// c tomb elem osszege
+let sumC = 0
+for (let e of c) {
+    sumC += e
+}
+
+console.log("C tomb elemeinek osszege: " + sumC)
+
+// b tomb elemeinek osszege
+let sumB = 0
+for (let e of b) {
+    if (typeof e == 'number') {
+        sumB += e
+    }
+        
+}
+console.log(b)
+console.log("B tomb elemeinek osszege: " + sumB)
+
+
+// Egyenlosegek
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(a == c) // false
+console.log(a === c) // false
+console.log(a == b) // true
+console.log(a === b) // true, mert b memoria szerint volt atadva a-ba
+
+// beszuras a seggere
+c[111] = 1
+console.log(c.length)
+c[c.length] = "asd"
+console.log(c)
+
+// fuggveny amely visszaadja hany ures elem van 
+function empty_elements(t) {
+    let s = 0
+    for (e of t) {
+        if (e == undefined) 
+            s += 1
+    }
+    return s
+}
+
+console.log(empty_elements(c))
+
+// tomb lenght tulajdonsaganak megvaltoztatasa
+console.log(a.length)
+a.length = 100
+console.log(a)
+a.length = 5
+console.log(a)
+a.length = 0
+console.log(a)
+a.length = 10
+console.log(a)
+
+// toString
+console.log(c)
+console.log(c.toString())
